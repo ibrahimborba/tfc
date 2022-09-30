36fdcef74fbf4160bb72195c8918aa9f3a535e3e
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-09-30 19:08:34 -0300

    feat: add status code 500 option to error middleware

--- app/backend/src/middlewares/error.middleware.ts
+++ app/backend/src/middlewares/error.middleware.ts
@@ -1,10 +1,9 @@
 import { ErrorRequestHandler } from 'express';
 
-const errorMiddleware: ErrorRequestHandler = (err, _req, res, next) => {
+const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
   const { status, message } = err;
   console.error(err);
-  res.status(status).json({ message });
-  next();
+  return res.status(status || 500).json({ message });
 };
 
 export default errorMiddleware;
