b8cc553aae11c36872715521fa48d75fc8392625
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-09-30 19:41:03 -0300

    refactor: simplify import path

--- app/backend/src/tests/error.unit.test.ts
+++ app/backend/src/tests/error.unit.test.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import * as sinon from 'sinon';
 import * as chai from 'chai'; 
-import errorMiddleware from '../../src/middlewares/error.middleware';
+import errorMiddleware from '../middlewares/error.middleware';
 import ValidationError from '../errors/ValidationError';
 
 const { expect } = chai;
@@ -18,19 +18,19 @@ describe('Error middleware', function () {
 
   after(() => { sinon.restore(); });
 
-  it('error without status code return status 500', async function () {
+  it('error without status code return status 500', () => {
     const error500 = new Error('Error without status');
     errorMiddleware(error500, req, res, next);
     expect((res.status as sinon.SinonStub).calledWith(500)).to.be.equal(true);
   });
 
-  it('error with status code return status', async function () {
+  it('error with status code return status', () => {
     const error500 = new ValidationError(400, 'Error with status');
     errorMiddleware(error500, req, res, next);
     expect((res.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
   });
 
-  it('returns error messsage', async function () {
+  it('returns error messsage', () => {
     const error500 = new Error('Error without status');
     errorMiddleware(error500, req, res, next);
     expect((res.json as sinon.SinonStub)
