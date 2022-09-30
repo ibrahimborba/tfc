707ec071bbec3aefa51901c1a34b396df0b8f341
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-09-30 19:03:23 -0300

    test: add unit test for error middleware

---
+++ app/backend/src/tests/error.unit.test.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from 'express';
+import * as sinon from 'sinon';
+import * as chai from 'chai'; 
+import errorMiddleware from '../../src/middlewares/error.middleware';
+import ValidationError from '../errors/ValidationError';
+
+const { expect } = chai;
+
+describe('Error middleware', function () {
+  const req = {} as Request;
+  const res = {} as Response;
+  let next: NextFunction;
+
+  before(() => {
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  after(() => { sinon.restore(); });
+
+  it('error without status code return status 500', async function () {
+    const error500 = new Error('Error without status');
+    errorMiddleware(error500, req, res, next);
+    expect((res.status as sinon.SinonStub).calledWith(500)).to.be.equal(true);
+  });
+
+  it('error with status code return status', async function () {
+    const error500 = new ValidationError(400, 'Error with status');
+    errorMiddleware(error500, req, res, next);
+    expect((res.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+  });
+
+  it('returns error messsage', async function () {
+    const error500 = new Error('Error without status');
+    errorMiddleware(error500, req, res, next);
+    expect((res.json as sinon.SinonStub)
+      .calledWith({ message:'Error with status' })).to.be.equal(true);
+  });
+
+});
