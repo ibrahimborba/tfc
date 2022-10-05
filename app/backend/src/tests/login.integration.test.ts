da2ea9bd7f842073bb181d917478db16f6461263
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-10-05 15:43:06 -0300

    feat: add mock to login integration tests

--- app/backend/src/tests/login.integration.test.ts
+++ app/backend/src/tests/login.integration.test.ts
@@ -4,7 +4,8 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import { app } from '../app';
 import { Response } from 'superagent';
-import { response } from 'express';
+import User from '../database/models/user';
+import * as UserMock from './mocks/User.json';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -12,130 +13,147 @@ const { expect } = chai;
 describe('Test login routes', () => {
   let chaiHttpResponse: Response;
 
-  describe('POST /login valid user', () => {
-    
-    before(async () => {
-      chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-       email: 'user@user.com',
-       password: 'secret_user',
-     });
-    });
-
-    it('returns status code 200', async () => {
-      expect(chaiHttpResponse).to.have.status(200);
+  describe('Success cases', () => {
+    before(() => {
+      sinon.stub(User, 'findOne').resolves(UserMock as User)
     });
   
-    it('returns a token', async () => {  
-      expect(chaiHttpResponse.body).to.have.property('token');
-    });
+    after(() => { sinon.restore(); });
+
+    describe('POST /login valid user', () => {
+    
+      before(async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+         email: 'user@user.com',
+         password: 'secret_user',
+       });
+      });
+  
+      it('returns status code 200', async () => {
+        expect(chaiHttpResponse).to.have.status(200);
+      });
+    
+      it('returns a token', async () => {  
+        expect(chaiHttpResponse.body).to.have.property('token');
+      });
+    })
+
+    describe('GET /login/validate valid user', () => {
+      before(async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+         email: 'user@user.com',
+         password: 'secret_user',
+       });
+      });
+  
+      it('returns status code 200', async () => {
+        let validateResponse = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', chaiHttpResponse.body.token);
+  
+        expect(validateResponse).to.have.status(200);
+      });
+    
+      it('returns a token', async () => {  
+        let validateResponse = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', chaiHttpResponse.body.token);
+        
+        expect(validateResponse.body).to.have.property('role');
+      });
+    })
+
   })
 
-  describe('POST /login empty email field', () => {
-    
-    before(async () => {
-      chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-       email: '',
-       password: 'secret_user',
-     });
-    });
-
-    it('returns status code 400', async () => {
-      expect(chaiHttpResponse).to.have.status(400);
+  describe('Error cases', () => {
+    before(() => {
+      sinon.stub(User, 'findOne').resolves(null)
     });
   
-    it('returns a message', async () => {  
-      expect(chaiHttpResponse.body).to.have.property('message');
-    });
-  })
+    after(() => { sinon.restore(); });
 
-  describe('POST /login inexistent user', () => {
+    describe('POST /login empty email field', () => {
     
-    before(async () => {
-      chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-       email: 'usuario@user.com',
-       password: 'secret_user',
-     });
-    });
-
-    it('returns status code 401', async () => {
-      expect(chaiHttpResponse).to.have.status(401);
-    });
-    it('returns a message', async () => {  
-      expect(chaiHttpResponse.body).to.have.property('message');
-    });
-  })
-
-  describe('GET /login/validate valid user', () => {
-    before(async () => {
-      chaiHttpResponse = await chai
-      .request(app)
-      .post('/login')
-      .send({
-       email: 'user@user.com',
-       password: 'secret_user',
-     });
-    });
-
-    it('returns status code 200', async () => {
-      let validateResponse = await chai
-      .request(app)
-      .get('/login/validate')
-      .set('authorization', chaiHttpResponse.body.token);
-
-      expect(validateResponse).to.have.status(200);
-    });
+      before(async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+         email: '',
+         password: 'secret_user',
+       });
+      });
   
-    it('returns a token', async () => {  
-      let validateResponse = await chai
-      .request(app)
-      .get('/login/validate')
-      .set('authorization', chaiHttpResponse.body.token);
+      it('returns status code 400', async () => {
+        expect(chaiHttpResponse).to.have.status(400);
+      });
+    
+      it('returns a message', async () => {  
+        expect(chaiHttpResponse.body).to.have.property('message');
+      });
+    })
+  
+    describe('POST /login inexistent user', () => {
       
-      expect(validateResponse.body).to.have.property('role');
-    });
-  })
-
-  describe('GET /login/validate without token', () => {
-    before(async () => {
-      chaiHttpResponse = await chai
-      .request(app)
-      .get('/login/validate')
-      .set('authorization', '');
-    });
-
-    it('returns status code 401', async () => {
-      expect(chaiHttpResponse).to.have.status(401);
-    });
+      before(async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({
+         email: 'usuario@user.com',
+         password: 'secret_user',
+       });
+      });
   
-    it('returns a message', async () => {  
-      expect(chaiHttpResponse.body).to.have.property('message');
-    });
-  })
-
-  describe('GET /login/validate with invalid token', () => {
-    before(async () => {
-      chaiHttpResponse = await chai
-      .request(app)
-      .get('/login/validate')
-      .set('authorization', 'invalidtoken');
-    });
-
-    it('returns status code 401', async () => {
-      expect(chaiHttpResponse).to.have.status(401);
-    });
+      it('returns status code 401', async () => {
+        expect(chaiHttpResponse).to.have.status(401);
+      });
+      it('returns a message', async () => {  
+        expect(chaiHttpResponse.body).to.have.property('message');
+      });
+    })
   
-    it('returns a message', async () => {  
-      expect(chaiHttpResponse.body).to.have.property('message');
-    });
-  })
+    describe('GET /login/validate without token', () => {
+      before(async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', '');
+      });
+  
+      it('returns status code 401', async () => {
+        expect(chaiHttpResponse).to.have.status(401);
+      });
+    
+      it('returns a message', async () => {  
+        expect(chaiHttpResponse.body).to.have.property('message');
+      });
+    })
+  
+    describe('GET /login/validate with invalid token', () => {
+      before(async () => {
+        chaiHttpResponse = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', 'invalidtoken');
+      });
+  
+      it('returns status code 401', async () => {
+        expect(chaiHttpResponse).to.have.status(401);
+      });
+    
+      it('returns a message', async () => {  
+        expect(chaiHttpResponse.body).to.have.property('message');
+      });
+    })
 
+  })
 });
